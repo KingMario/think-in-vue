ba83d296ee384c495a79f0024cf891b2acd0f0ad
KingMario <gcyyq@hotmail.com>
2017-02-28 20:17:48 +0800

    Runtime-only build caused bug

---
+++ standalone-vs-runtime-only-build/project/index.js
@@ -0,0 +1,9 @@
+import Vue from 'Vue'
+import MyComponent from './components/MyComponent'
+
+new Vue({
+  el: '#app',
+  components: {
+    MyComponent
+  }
+})
---
+++ standalone-vs-runtime-only-build/project/webpack.config.js
@@ -0,0 +1,38 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var name = require('./package').name
+
+module.exports = {
+  entry: path.resolve(__dirname, 'index.js'),
+  output: {
+    path: path.resolve(__dirname, `../../docs/${name}/0`),
+    publicPath: '',
+    filename: 'index.js'
+  },
+  resolve: {
+    extensions: ['.js', '.vue']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        },
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'index.html'),
+      inject: true
+    })
+  ]
+}
